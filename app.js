5441d680b33262e75334f665175243ad365aa85d
zerodivide85 <hasani.rogers@afrotechnologies.com>
2017-11-20 18:34:02 -0500

    updated styles

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
 // setup views
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -27,36 +27,36 @@ app.use(express.static(path.join(__dirname, 'app')));
 // app.use('/', routes);
 
 /// catch 404 and forward to error handler
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use((req, res, next) => {
+//     const err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
-/// error handlers
+// /// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
-}
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//     app.use((err, req, res, next) => {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err,
+//             title: 'error'
+//         });
+//     });
+// }
 
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
-});
+// // production error handler
+// // no stacktraces leaked to user
+// app.use((err, req, res, next) => {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {},
+//         title: 'error'
+//     });
+// });
 
 module.exports = app;
